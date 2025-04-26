c264bced5fdbb01a0c67b1cb4d7c62153c79505f
Thiago Lima <thiagollima1@hotmail.com>
2025-04-26 17:22:22 -0300

    ajustando erro ao receber xlsx do front

--- src/controllers/parseXlsx.ts
+++ src/controllers/parseXlsx.ts
@@ -6,29 +6,21 @@ import { regexTicker, regexProduto } from "../helpers/regexHelpers.js";
 import { formatDate, formatValue } from "../helpers/formatHelpers.js";
 
 const prisma = new PrismaClient();
-const userId = 1;
 
-await prisma.user.create({
-  data: {
-    email: "teste@email.com",
-    name: "Usuário Teste",
-    lastName: "Teste",
-    passwordHash: "senhateste",
-  },
-});
 export const parserXlsx = async (buffer: Buffer) => {
   const workbook = XLSX.read(buffer, { type: "buffer" });
   const sheetName = workbook.SheetNames[0];
   const sheet = workbook.Sheets[sheetName];
   const data: RawTransaction[] = XLSX.utils.sheet_to_json(sheet);
   parseTransations(data);
-  return data;
+  return "Operações enviadas";
 };
 
 const parseTransations = async (data: RawTransaction[]) => {
   const assetCache = new Map<string, number>();
   let assetId: number | undefined;
   const transactionsArray = [];
+  const userId = 1;
 
   for (const transacao of data) {
     const ticker: string = regexTicker(transacao["Produto"]);
@@ -87,6 +79,6 @@ const parseTransations = async (data: RawTransaction[]) => {
   await prisma.transaction.createMany({
     data: transactionsArray,
   });
-
+  console.log(transactionsArray);
   return transactionsArray;
 };
