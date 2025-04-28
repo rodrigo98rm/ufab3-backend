87775b8b2e0f40864ccd5d70211124f9683b6dea
Rodrigo Mayer <mayerrodrigo98@gmail.com>
2025-04-28 00:07:27 -0300

    Endpoint IR Asset

--- src/routes/ir/ir.controller.ts
+++ src/routes/ir/ir.controller.ts
@@ -1,15 +1,178 @@
-import { Router } from "express";
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { Request, Response, Router } from "express";
 
+import { Prisma } from "../../../generated/prisma";
 import { prisma } from "../prisma";
 
 const router = Router();
 
-router.get("/", async (req, res) => {
-  const result = await prisma.transaction.findMany();
+const assetSelect = {
+  category: true,
+  name: true,
+  ticker: true,
+} satisfies Prisma.AssetSelect;
 
-  res.status(200).json({
-    transactions: result,
+type Asset = Prisma.AssetGetPayload<{ select: typeof assetSelect }>;
+
+router.get("/", async (req: Request, res: Response) => {
+  const userId = 1; // TODO: get userId from request
+
+  const result = await prisma.transaction.findMany({
+    distinct: ["assetId"],
+    select: {
+      asset: true,
+    },
+    where: {
+      asset: {
+        category: {
+          not: null,
+        },
+      },
+      userId,
+    },
   });
+
+  const assets = {
+    fiis: [] as Asset[],
+    stocks: [] as Asset[],
+  };
+
+  result.forEach((transaction) => {
+    const asset = transaction.asset;
+    if (asset.category === "FII") {
+      assets.fiis.push(asset);
+    } else if (asset.category === "ACAO") {
+      assets.stocks.push(asset);
+    }
+  });
+
+  res.json({
+    assets,
+  });
+});
+
+router.get("/:ticker", async (req: Request, res: Response) => {
+  const userId = 1; // TODO: get userId from request
+
+  const { ticker } = req.params;
+
+  const transactions = await prisma.transaction.findMany({
+    orderBy: {
+      executedAt: "asc",
+    },
+    where: {
+      asset: {
+        ticker,
+      },
+      type: "Transferência - Liquidação",
+      userId,
+    },
+  });
+
+  let averagePrice = 0;
+  let currentQuantity = 0;
+  transactions.forEach((transaction) => {
+    // Only purchases influence the average price
+    if (transaction.side === "C") {
+      const totalValue =
+        currentQuantity * averagePrice +
+        transaction.amount.toNumber() * transaction.value.toNumber();
+      currentQuantity += transaction.amount.toNumber();
+      averagePrice = totalValue / currentQuantity;
+    }
+  });
+
+  const asset = await prisma.asset.findFirst({
+    where: {
+      ticker,
+    },
+  });
+
+  let totalQuantity = 0;
+
+  transactions.forEach((transaction) => {
+    if (transaction.side === "C") {
+      totalQuantity += transaction.amount.toNumber();
+    } else {
+      totalQuantity -= transaction.amount.toNumber();
+    }
+  });
+
+  interface Response {
+    avgPrice: number;
+    cnpj: string;
+    code: string;
+    description: string;
+    earnings: number;
+    group: string;
+    location: string;
+    origin: string;
+    positions: {
+      value: number;
+      year: number;
+    }[];
+    ticker: string;
+    type: string;
+  }
+
+  if (asset?.category === "FII") {
+    const response: Response = {
+      avgPrice: averagePrice,
+      cnpj: asset.cnpj || "000.000.000/0000-00",
+      code: "03 - Fundos de Investimento Imobiliário",
+      description: `${totalQuantity} cotas do FII de código ${asset.ticker} a um preço médio de R$ ${averagePrice.toFixed(2).replace(".", ",")}`,
+      earnings: 0,
+      group: "07 - Fundos",
+      location: "Brasil",
+      origin: "Brasil",
+      positions: [
+        {
+          value: 0,
+          year: 2024,
+        },
+        {
+          value: 0,
+          year: 2025,
+        },
+      ],
+      ticker: asset.ticker,
+      type: "FII",
+    };
+
+    res.json(response);
+    return;
+  } else if (asset?.category === "ACAO") {
+    const response: Response = {
+      avgPrice: averagePrice,
+      cnpj: asset.cnpj || "000.000.000/0000-00",
+      code: "01 - Ações (inclusive as listadas em Bolsa)",
+      description: `${totalQuantity} ações da empresa ${asset.ticker} a um preço médio de R$ ${averagePrice.toFixed(2).replace(".", ",")}`,
+      earnings: 0,
+      group: "03 - Participações Societárias",
+      location: "Brasil",
+      origin: "Brasil",
+      positions: [
+        {
+          value: 0,
+          year: 2024,
+        },
+        {
+          value: 0,
+          year: 2025,
+        },
+      ],
+      ticker: asset.ticker,
+      type: "ACAO",
+    };
+
+    res.json(response);
+    return;
+  }
+
+  console.log(transactions);
 });
 
 export default router;
