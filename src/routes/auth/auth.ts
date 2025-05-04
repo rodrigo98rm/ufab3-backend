f1b1d360e871957431120cbaffe37088d096ed4e
Rodrigo Mayer <mayerrodrigo98@gmail.com>
2025-05-04 11:43:21 -0300

    Login fix

--- src/routes/auth/auth.ts
+++ src/routes/auth/auth.ts
@@ -1,27 +1,40 @@
+import { compareSync, hashSync } from "bcrypt";
 import { Router } from "express";
 import { Request, Response } from "express";
-import { hashSync, compareSync } from "bcrypt";
-import { prisma } from "../prisma";
 import jwt from "jsonwebtoken";
+
+import { prisma } from "../prisma";
 
 export const JWT_SECRET = process.env.JWT_SECRET!;
 
 const authRoutes: Router = Router();
 
+interface SingUpBody {
+  email: string;
+  lastName: string;
+  name: string;
+  password: string;
+}
+
 authRoutes.post("/sign", async (req: Request, res: Response) => {
-  const { email, lastName, name, password } = req.body;
+  const { email, lastName, name, password } = req.body as SingUpBody;
 
   const user = await prisma.user.findFirst({ where: { email } });
   if (user) {
-    res.status(400).json({error:"Email ja existe!"});
-    return
+    res.status(400).json({ error: "Email ja existe!" });
+    return;
   }
-  const { createdAt, updatedAt, passwordHash: _, ...rest} = await prisma.user.create({
+  const {
+    createdAt,
+    passwordHash: _,
+    updatedAt,
+    ...rest
+  } = await prisma.user.create({
     data: {
       email,
       lastName,
       name,
-      passwordHash: hashSync(password as string, 10),
+      passwordHash: hashSync(password, 10),
     },
   });
 
@@ -30,17 +43,22 @@ authRoutes.post("/sign", async (req: Request, res: Response) => {
   res.json(response);
 });
 
-authRoutes.post("/login", async (req: Request, res: Response) => {
-  const { email, password } = req.body;
+interface LoginBody {
+  email: string;
+  password: string;
+}
 
-  let user = await prisma.user.findFirst({ where: { email } });
+authRoutes.post("/login", async (req: Request, res: Response) => {
+  const { email, password } = req.body as LoginBody;
+
+  const user = await prisma.user.findFirst({ where: { email } });
   if (!user) {
-    res.status(400).json({error:"Email nao existe!"});
-    return
+    res.status(400).json({ error: "Email nao existe!" });
+    return;
   }
-  
+
   if (!compareSync(password, user.passwordHash)) {
-    res.status(400).json({error:"Senha incorreta!"});
+    res.status(400).json({ error: "Senha incorreta!" });
     return;
   }
   const token = jwt.sign(
@@ -50,6 +68,6 @@ authRoutes.post("/login", async (req: Request, res: Response) => {
     JWT_SECRET,
   );
 
-  res.json({ user, token });
+  res.json({ token, user });
 });
 export default authRoutes;
